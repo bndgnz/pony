c4181b4d87090d2f7d5d165bab90cac3f5740502
bndgnz <61714487+bndgnz@users.noreply.github.com>
2024-08-12 08:23:48 +1200

    tidy ups

--- src/components/common/content.tsx
+++ src/components/common/content.tsx
@@ -2,55 +2,46 @@ import Richtext from "@/src/utils/helpers/richTextHelper";
 import Image from "next/image";
 import Link from "next/link";
 
-
-function Content(props:any) {
-  console.log(props)
-   if (props.props.showContent ===true) {
-
-    function ContentImage ()  { 
-
- if (props.props.contentImage) {return (               
-  <Image src={props.props.contentImage[0].secure_url} width={props.props.contentImage[0].width} height={props.props.contentImage[0].height} alt={props.props.contentImage[0].alt} />
-           
-           )}
-else {null;}
-
-
+function Content(props: any) {
+  if (props.props.showContent === true) {
+    function ContentImage() {
+      if (props.props.contentImage) {
+        return (
+          <Image
+            src={props.props.contentImage[0].secure_url}
+            width={props.props.contentImage[0].width}
+            height={props.props.contentImage[0].height}
+            alt={props.props.contentImage[0].alt}
+          />
+        );
+      } else {
+        null;
+      }
     }
 
     return (
-        <section className="section about about--alt">
-          <div className="container">
-            <div className="row section__row align-items-center">
-              <div className="col-lg-6 col-xl-6 section__col">
-                <div className="section__content">
-                          <div className="about__section-inner">
-                    
-                  <Richtext
-                  content={
-                    props.props.content
-                  }
-                />
-
-                  </div>
-                 
-                </div>
-              </div>
-              <div className="col-lg-6 col-xl-5 offset-xl-1 section__col">
-                <div className="content-image" data-wow-duration="0.4s">
-
-                  <ContentImage />
-          
+      <section className="section about about--alt">
+        <div className="container">
+          <div className="row section__row align-items-center">
+            <div className="col-lg-6 col-xl-6 section__col">
+              <div className="section__content">
+                <div className="about__section-inner">
+                  <Richtext content={props.props.content} />
                 </div>
               </div>
             </div>
+            <div className="col-lg-6 col-xl-5 offset-xl-1 section__col">
+              <div className="content-image" data-wow-duration="0.4s">
+                <ContentImage />
+              </div>
+            </div>
           </div>
-        </section>
-      );
-    }
-
-    else{null}
-
+        </div>
+      </section>
+    );
+  } else {
+    null;
+  }
 }
 
 export default Content;

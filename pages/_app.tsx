9588f1a59de28fc366e63844e76faaa496883c8f
bndgnz <61714487+bndgnz@users.noreply.github.com>
2024-09-30 19:54:53 +1300

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -21,35 +21,33 @@ import {
 } from "@apollo/client";
 
 const dataEnvironment = process.env.NEXT_PUBLIC_CONTENTFUL_ENVIRONMENT;
+ 
 
 function MyApp({ Component, pageProps, data }: any) {
   const client = new ApolloClient({
     uri:
-      "https://graphql.contentful.com/content/v1/spaces/dgdm78o9p3tb" +
+      "https://graphql.contentful.com/content/v1/spaces/dgdm78o9p3tb"+
       "?access_token=9S2SpOEemYLtYlWCBeIRtKxFEmaUcNWG484otyL8wNU",
     cache: new InMemoryCache(),
   });
   return (
-    <>
-      <script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=G-K6H04V4F05"
-      ></script>
-      {`
-        window.dataLayer = window.dataLayer || [];
+   
+
+<>
+   <script async src="https://www.googletagmanager.com/gtag/js?id=G-K6H04V4F05"></script>
+<script>
+   {`window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'G-K6H04V4F05');
-      `}
-      <script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=G-K6H04V4F05"
-      ></script>
+  gtag('config', 'G-K6H04V4F05');`}
+</script>
 
+       
       <ApolloProvider client={client}>
-        <Component {...pageProps} />
-
+     
+          <Component {...pageProps} />
+ 
         {/* Preloader */}
 
         {/* Go Top Button */}

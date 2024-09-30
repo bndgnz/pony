86b90abd51a8f33ad81d551751c6ca656fb0da46
bndgnz <61714487+bndgnz@users.noreply.github.com>
2024-09-30 19:49:20 +1300

    analytics

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -15,8 +15,7 @@ const Layout = (props:any) => {
     <>
       {/* Preloader */}
     
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-557JBPTS"
-height="0" width="0" style={{display:'none', visibility:'hidden'}}></iframe></noscript>
+ 
  
       <NavBar />
       <NavBarBanner props={props} />

4d285bb7337cb77a846628eba83ba61ebae7b795
bndgnz <61714487+bndgnz@users.noreply.github.com>
2024-10-04 09:41:04 +1300

    social media embed and sitemap

--- src/components/facebook.tsx
+++ src/components/facebook.tsx
@@ -1,9 +1,11 @@
-
+import { FacebookEmbed } from 'react-social-media-embed';
 
 
 const facebook = (props: any) =>{
 
-    return (<p> </p>)
+    return (<div style={{ display: 'flex', justifyContent: 'center' }}>
+        <FacebookEmbed url="https://www.facebook.com/andrewismusic/posts/451971596293956" width={550} />
+      </div>)
 
 
 
